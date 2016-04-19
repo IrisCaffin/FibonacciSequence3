b1b149f74492816ecdaa701628f3263a7681c7c9
Iris Caffin <iriscaffin@gmail.com>
2016-04-19 12:26:09 -0700

    Fibonacci Sequence another recursive method in C#

---
+++ FibonacciSequence3/FibonacciSequence3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSequence3
+{
+    class Program
+    {
+        public static void FibonacciRecursive
+            (
+            int firstnumber,
+            int secondnumber,
+            int counter,
+            int lenght
+            )
+        {
+            if (counter <= lenght)
+            {
+                Console.Write("{0} ", firstnumber);
+                FibonacciRecursive(secondnumber, firstnumber + secondnumber, counter + 1, lenght);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the length of the Fibonacci Series: ");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+            FibonacciRecursive(0, 1, 1, lenght);
+            Console.ReadKey();
+        }
+    }
+}
